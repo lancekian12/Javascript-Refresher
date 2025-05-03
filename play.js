eedc460f3239b0269a0bafd478b8489d854566b2
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2025-05-03 11:13:32 +0800

    referesher for object and this keyword rule

--- play.js
+++ play.js
@@ -1,24 +1,34 @@
-const name = "Max";
-var age = 29;
-const hasHobbies = true;
+// const name = "Max";
+// var age = 29;
+// const hasHobbies = true;
 
-age = 30;
+// age = 30;
 
-const summarize = (userName, userAge, userHasHobby) => {
-  return `Name is ${userName}, age is ${userAge} and the user has hobbies: ${userHasHobby}`;
+// const summarize = (userName, userAge, userHasHobby) => {
+//   return `Name is ${userName}, age is ${userAge} and the user has hobbies: ${userHasHobby}`;
+// };
+
+// const add = (a, b) => a + b;
+// console.log(add(1, 2));
+
+// const addOne = (a) => a + 1;
+
+// console.log(addOne(1));
+
+// const addRandom = () => 1 + 2;
+// console.log(addRandom());
+
+// console.log(summarize("kian", 22, "gym"));
+// console.log(summarize(name, age, hasHobbies));
+
+// console.log(name);
+
+const person = {
+  name: "lance kian",
+  age: 29,
+  greet() {
+    console.log("Hi, I am " + this.name);
+  },
 };
 
-const add = (a, b) => a + b;
-console.log(add(1, 2));
-
-const addOne = (a) => a + 1;
-
-console.log(addOne(1));
-
-const addRandom = () => 1 + 2;
-console.log(addRandom());
-
-console.log(summarize("kian", 22, "gym"));
-console.log(summarize(name, age, hasHobbies));
-
-console.log(name);
+console.log(person.greet());
